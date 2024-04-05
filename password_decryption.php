ac16a66a907eb404f4f16df2363b75e216b6cf74
rounak-codes <rounakbag24@gmail.com>
2024-04-05 20:25:00 +0530

    0.4 decryption update

--- password_decryption.php
+++ password_decryption.php
@@ -1,16 +1,12 @@
 <?php
 // Include phpseclib3 autoloader
 require 'vendor/autoload.php';
-include_once 'db_connect.php';
 
 use phpseclib3\Crypt\AES;
 
 // Function to decrypt the password
 function decryptPassword($encryptedData, $encryptionKey) {
-    // Decode the base64 encoded encrypted data
-    $encryptedData = base64_decode($encryptedData);
-    
-    // Extract IV and encrypted password from the decrypted data
+    // Extract IV and encrypted password from the encrypted data
     $iv = substr($encryptedData, 0, 16); // IV length is 16 bytes
     $encryptedPassword = substr($encryptedData, 16);
     
